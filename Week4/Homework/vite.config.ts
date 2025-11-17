e8180370a6afd7015d10f497c2208b1e6fbcfe3e
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-18 00:27:22 +0900

    chore: 절대 경로 설정

--- Week4/Homework/vite.config.ts
+++ Week4/Homework/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
+});
