e051c9426288ad7e35541fbde87e6b9c93561a3a
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-18 05:10:20 +0900

    design: 공통 컴포넌트 FormHeader 구현

---
+++ Week4/Homework/src/shared/ui/form-header/FormHeader.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css";
+import { vars } from "@/shared/styles/token.css";
+
+export const formHeader = style({
+  fontSize: vars.font.size.s20,
+  fontWeight: vars.font.weight.bold,
+});
