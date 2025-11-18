b4b5f1a56d307a1049bb0d4c64a429ffc9f98d92
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-18 21:14:47 +0900

    feat: 사용자 정보 요청 방식 변경

--- Week4/Homework/src/features/my-page/api/getUser.ts
+++ Week4/Homework/src/features/my-page/api/getUser.ts
@@ -1,8 +1,7 @@
 import { axiosApi } from "@/shared/api";
 import type { User } from "@/entities/user/model/user";
 
-export const getUser = async () => {
-  const id = localStorage.getItem("userId");
+export const getUser = async (id: string) => {
   const res: User = await axiosApi.get(`/api/v1/users/${id}`);
   return res;
 };
