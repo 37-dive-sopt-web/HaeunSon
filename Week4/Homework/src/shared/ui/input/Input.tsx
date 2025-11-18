55d173defceb0ce8dc30517669dee9b68670e994
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-18 17:09:24 +0900

    feat: props에 name, value 추가

--- Week4/Homework/src/shared/ui/input/Input.tsx
+++ Week4/Homework/src/shared/ui/input/Input.tsx
@@ -2,18 +2,29 @@ import type { ChangeEvent } from "react";
 import * as s from "./Input.css";
 
 interface InputProps {
+  name?: string;
+  value?: string;
   label: string;
   placeholder: string;
   type: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ label, placeholder, type, onChange }: InputProps) => {
+const Input = ({
+  name,
+  value,
+  label,
+  placeholder,
+  type,
+  onChange,
+}: InputProps) => {
   return (
     <div className={s.layout}>
       <label>{label}</label>
       <input
         className={s.input}
+        name={name}
+        value={value}
         type={type}
         placeholder={placeholder}
         onChange={onChange}
