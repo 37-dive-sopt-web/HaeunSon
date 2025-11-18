5ae255490210db5d8458b1cb524f6b806b3c4877
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-18 17:47:45 +0900

    chore: 절대 경로 추가

--- Week4/Homework/vite.config.ts
+++ Week4/Homework/vite.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "src"),
       "@app": path.resolve(__dirname, "src/app"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@widgets": path.resolve(__dirname, "src/widgets"),
       "@shared": path.resolve(__dirname, "src/shared"),
