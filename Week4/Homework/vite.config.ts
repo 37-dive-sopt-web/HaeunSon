04ff02a28cf4029053817a75089b38aae07ef2d5
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-18 17:07:30 +0900

    chore: vite-plugin-svgr 설정

--- Week4/Homework/vite.config.ts
+++ Week4/Homework/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
